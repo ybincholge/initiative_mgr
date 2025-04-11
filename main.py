52f2f03dba10cfedb4617949aed7b503ed26911f
Yongbin Cho <ybin.cho@lge.com>
2025-04-11 15:30:44 +0900

    testdebug2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import pandas as pd
+import pandas as pd
 
 view = [100, 150, 30]
 st.title("# My initiative manager")
@@ -9,5 +9,5 @@ st.write('## bar chart')
 st.bar_chart(view)
 
 
-#sview = pd.Series(view)
-#sview
+sview = pd.Series(view)
+sview
