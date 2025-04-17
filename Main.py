2f88808aac7264ffb4e0e3aa5415e66c4df41fa1
Yongbin Cho <ybin.cho@lge.com>
2025-04-18 08:40:54 +0900

    need check not connected to browser

--- Main.py
+++ Main.py
@@ -1,20 +1,37 @@
 import streamlit as st 
 
-# Page title and layout settings
-st.set_page_config(page_title="Initiative Manager", layout="wide")
- 
-# Logo image path
-LOGO_PATH = "logo.png"  # Replace with the actual path to your logo image
+st.set_page_config(layout="wide")
+st.logo("logo.png", size = 'large')
 
-# Sidebar settings
-st.sidebar.image(LOGO_PATH, use_container_width=True)  # Display the logo image
+p1 = st.Page("pages/1 Initiatives.py", title = "Initiatives")
+p2 = st.Page("pages/2 Sprint Planning.py", title = "Sprint Planning")
+p3 = st.Page("pages/3 Sprint Review.py", title = "Sprint Review")
+# p4 = st.Page("pages/5 Account Setting.py", title = "Account Setting")
+p4 = st.Page("pages/6 Organization.py", title = "Organization")
 
-# Sidebar menu
-st.sidebar.subheader("Pages")
-st.sidebar.write("Select a page from the menu above.")
+pg = st.navigation([p1, p2, p3, p4 ])
 
-# Main page content
-st.title("Initiative Manager")
-st.write("Welcome to the Initiative Manager!")
+pg.run()
 
-# Add your page content here
+
+st.title("Main")
+# ID 입력 필드
+user_id = st.text_input("ID", value=st.session_state.get('user_id', ''))
+
+# Password 입력 필드 (mask 처리)
+password = st.text_input("Password", type="password", value=st.session_state.get('password', ''))
+
+# Role 선택 드롭다운
+role_options = ["실장", "팀장", "파트장", "개발자"]
+role = st.selectbox("Role", role_options, index=role_options.index(st.session_state.get('role', '개발자')))
+
+# Save button
+login_button = st.button("Log in")
+
+# If the save button is clicked
+if login_button:
+    # Store the values in session state
+    st.session_state["user_id"] = user_id
+    st.session_state["password"] = password
+    st.session_state["role"] = role
+    # st.success("Login saved!")
--- pages/3 Sprint Review.py
+++ pages/3 Sprint Review.py
@@ -1,12 +1,4 @@
 import streamlit as st
 
-# 페이지 기본 설정
-st.set_page_config(page_title="Initiative Manager", layout="wide")
-
-# 로고 이미지 경로
-LOGO_PATH = "logo.png"  # 로고 이미지 파일 경로를 설정하세요.
-
-# 왼쪽 사이드바 구성
-st.sidebar.image(LOGO_PATH, use_column_width=True)  # 로고 이미지 표시
-st.sidebar.title("Menu")
-st.sidebar.write("Select a page from the menu above.")
+st.title("Sprint Review")
+st.write("This page is under construction.")
